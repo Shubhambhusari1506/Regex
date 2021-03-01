1a278e44b7090f860a8976c95a99b6351c877af0
shubhambhusari <shubhambhusari2@gmail.com>
2021-03-01 19:24:07 +0530

    Added: enter password using Eight character

--- Firstname.java
+++ Firstname.java
@@ -6,26 +6,26 @@ public class Firstname {
 	static   Scanner name = new  Scanner (System.in); 
 				
 		
-	static void MobNumber() {
+	static void Pass() {
 		
-	String mobilenumber = name.nextLine();
+	String password = name.nextLine();
 	
-	// validate first name
-	  	boolean  user= Pattern.matches("^[0-9]{2}[ ][6-9]{1}[0-9]{9}$",mobilenumber );
+	// validate password
+	  	boolean  user= Pattern.matches("^[A-Za-z]{8}$",password);
 	
 	if (user == true ) {
-		System.out.println("you entered a valid mobile number  :" +mobilenumber);
-	}else { System.out.println ("Not a valid mobile number");
-			System.out.println("Enter valid mobile number");
-			MobNumber();
+		System.out.println("you entered a valid password  :" +password);
+	}else { System.out.println ("Not a valid password");
+			System.out.println("Enter valid password");
+			Pass();
 	}
 	
 	}
 	
 	
    public static void main( String[] args ) {
-	   System.out.println("enter mobile number");
-	   MobNumber();
+	   System.out.println("enter password using eight characters");
+	   Pass();
 
 }
 }
